6ff321a83a929d8edc75c14a0d01481f2eb6284c
Александр_П <mrtuzzz@gmail.com>
2022-11-27 22:43:16 +0500

    add if_else_max_min

--- sum_console/Program.cs
+++ sum_console/Program.cs
@@ -1,7 +1,14 @@
 ﻿int a = 5;
 int b = 7;
 int max = a;
-if (a > max) max = a;
-if (b > max) max = b;
+int min = a;
+
+if (a > max) max = a; else min = a;
+
+if (b > max) max = b; else min = b;
+
 Console.Write("max = ");
 Console.Write(max);
+
+Console.Write(", min = ");
+Console.Write(min);
