c6244b32fb6c60b99b43516c9c0575b1a750c1bc
Александр_П <mrtuzzz@gmail.com>
2022-11-28 00:10:14 +0500

    add homework_1

--- hello_console/Program.cs
+++ hello_console/Program.cs
@@ -1,8 +1,47 @@
-﻿Console.Write("Введите имя пользователя: ");
+﻿/* Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 if (username.ToLower() == "саша"){
  Console.Write("Ура, это Саша!");
 } else {
  Console.Write("Привет, ");
  Console.Write(username);
+} */
+
+Console.Clear();
+
+int xa = 40, ya = 1, 
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+
+while (count < 10000){
+ int what = new Random().Next(0,3);
+ if (what == 0) 
+ {
+  x = (x+xa)/2; 
+  y = (y+ya)/2;
+ }
+ if (what == 1) 
+ {
+  x = (x+xb)/2; 
+  y = (y+yb)/2;
+ }
+ if (what == 2) 
+ {
+  x = (x+xc)/2; 
+  y = (y+yc)/2;
+ }
+
+ Console.SetCursorPosition(x, y);
+ Console.WriteLine("+");
+ count++;
 }
--- sum_console/Program.cs
+++ sum_console/Program.cs
@@ -1,14 +1,92 @@
-﻿int a = 5;
-int b = 7;
-int max = a;
-int min = a;
+﻿// Задача 2
 
-if (a > max) max = a; else min = a;
+int a1 = 5;
+int b1 = 7;
 
-if (b > max) max = b; else min = b;
+int a2 = 2;
+int b2 = 10;
 
-Console.Write("max = ");
-Console.Write(max);
+int a3 = -9;
+int b3 = -3;
 
-Console.Write(", min = ");
-Console.Write(min);
+int max1 = a1;
+int min1 = a1;
+
+int max2 = a2;
+int min2 = a2;
+
+int max3 = a3;
+int min3 = a3;
+
+if (a1 > max1) max1 = a1; else min1 = a1;
+if (b1 > max1) max1 = b1; else min1 = b1;
+
+if (a2 > max2) max2 = a2; else min2 = a2;
+if (b2 > max2) max2 = b2; else min2 = b2;
+
+if (a3 > max3) max3 = a3; else min3 = a3;
+if (b3 > max3) max3 = b3; else min3 = b3;
+
+Console.WriteLine($"max1 = {max1}, min1 = {min1}, max2 = {max2}, min2 = {min2}, max3 = {max3}, min3 = {min3}");
+
+// Задача 4
+
+a1 = 2;
+b1 = 3;
+int c1 = 7;
+
+a2 = 44;
+b2 = 5;
+int c2 = 78;
+
+a3 = 22;
+b3 = 3;
+int c3 = 9;
+
+max1 = a1;
+max2 = a2;
+max3 = a3;
+
+if (a1 > max1) max1 = a1;
+if (b1 > max1) max1 = b1;
+if (c1 > max1) max1 = c1;
+
+if (a2 > max2) max2 = a2;
+if (b2 > max2) max2 = b2;
+if (c2 > max2) max2 = c2;
+
+if (a3 > max3) max3 = a3;
+if (b3 > max3) max3 = b3;
+if (c3 > max3) max3 = c3;
+
+Console.WriteLine($"max1 = {max1}, max2 = {max2}, max3 = {max3}");
+
+// Задача 6
+
+int n = 4;
+int x = n % 2;
+if (x == 0) Console.WriteLine($"{n} -> Да");
+else Console.WriteLine($"{n} -> Нет");
+
+n = -3;
+x = n % 2;
+if (x == 0) Console.WriteLine($"{n} -> Да");
+else Console.WriteLine($"{n} -> Нет");
+
+n = 7;
+x = n % 2;
+if (x == 0) Console.WriteLine($"{n} -> Да");
+else Console.WriteLine($"{n} -> Нет");
+
+// Задача 8
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= number; i++)
+{
+ if (i % 2 == 0) 
+ {
+  Console.WriteLine(i);
+ };
+};
