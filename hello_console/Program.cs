e5df75aa7e286a2ecd8ee6031876caea30d3a7d1
Александр_П <mrtuzzz@gmail.com>
2022-12-03 18:05:52 +0500

    add hello_console update

--- hello_console/Program.cs
+++ hello_console/Program.cs
@@ -1,4 +1,7 @@
-﻿/* Console.Write("Введите имя пользователя: ");
+﻿using static System.Console; // Теперь не нужно писать 'Console.'
+Clear();
+
+/* Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 if (username.ToLower() == "саша"){
  Console.Write("Ура, это Саша!");
@@ -7,9 +10,9 @@ if (username.ToLower() == "саша"){
  Console.Write(username);
 } */
 
-Console.Clear();
+//-----------------------------------------------
 
-int xa = 40, ya = 1, 
+/* int xa = 40, ya = 1, 
     xb = 1, yb = 30,
     xc = 80, yc = 30;
 
@@ -44,7 +47,9 @@ while (count < 10000){
  Console.SetCursorPosition(x, y);
  Console.WriteLine("+");
  count++;
-}
+} */
+
+//-----------------------------------------------
 
 /*
 int Max(int arg1, int arg2, int arg3)
@@ -64,6 +69,8 @@ int max = Max(
 
 Console.WriteLine(max);
 */
+
+//-----------------------------------------------
 
 /*
 void fillarr(int[] coll1)
@@ -112,3 +119,151 @@ Console.WriteLine("----------------");
 int posof = indexof(arr, 5);
 Console.WriteLine(posof);
 */
+
+//-----------------------------------------------
+
+// Методы
+// 1 Вид:
+
+/*
+void func1()
+{
+ WriteLine("Привет, мир!");
+}
+func1();
+
+
+// 2 Вид:
+
+void func2(string msg)
+{
+ WriteLine(msg);
+}
+//func2("Сообщение");
+
+void func2x(string msg, int count)
+{
+ int i = 0;
+ while (i < count)
+ {
+  WriteLine(msg + i);
+  i++;
+ }
+}
+func2x(msg: "Сообщение", count: 3);
+
+
+// 3 Вид:
+
+int func3()
+{
+ return DateTime.Now.Year;
+}
+int year = func3();
+WriteLine(year);
+
+
+// 4 Вид:
+
+string func4(int count, string x)
+{
+ 
+ string res = ""; // "" или String.Empty;
+ for (int i = 0; i < count; i++)
+ {
+  res = res + x;
+ }
+ return res;
+}
+
+string res = func4(5, "Текст");
+WriteLine(res);
+*/
+
+//-----------------------------------------------
+
+// Цикл в цикле:
+
+/*
+for (int i = 2; i <= 10; i++)
+{
+ for (int x = 2; x <= 10; x++)
+ {
+  WriteLine($"{i} * {x} = {i*x}");
+ }
+ WriteLine();
+}
+*/
+
+//-----------------------------------------------
+
+/*
+string text = "Быть знаменитым некрасиво."
++ "Не это подымает ввысь."
++ "Не надо заводить архива,"
++ "Над рукописями трястись."
++ "Цель творчества - самоотдача,"
++ "А не шумиха, не успех."
++ "Позорно, ничего не знача,"
++ "Быть притчей на устах у всех.";
+
+string replace(string text, char oldval, char newval)
+{
+ string res = "";
+ int len = text.Length;
+ for (int i = 0; i < len; i++)
+ {
+  if (text[i] == oldval) res = res + $"{newval}";
+  else res = res + $"{text[i]}";
+ }
+ return res;
+};
+
+string nextext = replace(text, ' ', '|');
+
+WriteLine(nextext);
+WriteLine();
+
+nextext = replace(nextext, 'к', 'К');
+
+WriteLine(nextext);
+*/
+
+//-----------------------------------------------
+
+// Сортировка чисел
+
+/*
+int[] arr = {2, 1, 4, 3, 8, 6, 5, 7, 1, 1};
+
+void printarr(int[] arrx)
+{
+ for (int i = 0; i < arrx.Length; i++)
+ {
+  Write($"{arrx[i]} ");
+ }
+ WriteLine();
+}
+
+void sort(int[] arrx)
+{
+ for (int i = 0; i < arrx.Length - 1; i++)
+ {
+  int minpos = i;
+  for (int x = i + 1; x < arrx.Length; x++)
+  {
+    if (arrx[x] < arrx[minpos]) minpos = x;
+  }
+  int temp = arrx[i];
+  arrx[i] = arrx[minpos];
+  arrx[minpos] = temp;
+ }
+}
+
+printarr(arr);
+sort(arr);
+printarr(arr);
+*/
+
+//-----------------------------------------------
+
