4691a65aa17ed357175372456ed2a25ae48fe0f7
Александр_П <mrtuzzz@gmail.com>
2022-12-03 15:49:39 +0500

    add homework_2 update

--- example_2/Program.cs
+++ example_2/Program.cs
@@ -1,67 +1,24 @@
-﻿/*
-int Max(int arg1, int arg2, int arg3)
-{
- int result = arg1;
- if (arg2 > result) result = arg2;
- if (arg3 > result) result = arg3;
- return result;
-};
+﻿using static System.Console;
+Clear();
 
-int[] arr = {15, 21, 33, 45, 89, 91, 8, 15, 29};
+// Задача 10 и 13
 
-int max = Max(
- Max(arr[0], arr[1], arr[2]), 
- Max(arr[3], arr[4], arr[5]), 
- Max(arr[6], arr[7], arr[8]));
+WriteLine("Введите трёхзначное число:" );
+String x = ReadLine();
 
-Console.WriteLine(max);
-*/
+if (x.Length >= 3) WriteLine($"Вторая цифра числа: {x[1]}");
+else WriteLine("Не трехзначное число!");
 
-/*
-void fillarr(int[] coll1)
-{
- int n = coll1.Length;
- int i = 0;
- while (i < n){
-  coll1[i] = new Random().Next(1, 10); 
-  i++;
- }
-}
+if (x.Length >= 3) WriteLine($"Третья цифра числа: {x[2]}");
+else WriteLine("Третьей цифры нет!");
 
-void printarr(int[] coll2)
-{
- int len = coll2.Length;
- int pos = 0;
- while (pos < len){
-  Console.WriteLine(coll2[pos]);
-  pos++;
- }
-}
+WriteLine("---------------------");
 
-int indexof(int[] coll, int find)
-{
- int count = coll.Length;
- int index = 0;
- int position = -1;
+// Задача 15
 
- while (index < count){
-  if (coll[index] == find)
-  {
-   position = index; 
-   break;
-  }
-  index++;
- }
- return position;
-}
+ WriteLine("Введите число от 1 до 7, обозначающее день недели:");
+ int n = Convert.ToInt32(ReadLine());
 
-int[] arr = new int[10];
-
-fillarr(arr);
-printarr(arr);
-Console.WriteLine("----------------");
-
-int posof = indexof(arr, 5);
-Console.WriteLine(posof);
-*/
-
+if (n == 7 || n == 6) WriteLine("Сегодня выходной");
+else if (n > 7 || n < 1 ) WriteLine("Вы указали не верное число");
+else WriteLine("Сегодня рабочий день");
