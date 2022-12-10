0a84a13f325484940680bcad05bfda1613d2790c
Александр_П <mrtuzzz@gmail.com>
2022-12-10 22:04:57 +0500

    add example_8

--- example_8/Program.cs
+++ example_8/Program.cs
@@ -5,26 +5,65 @@ Clear();
 
 double[,] arr = new double[3, 4];
 
-for (int i = 0; i < arr.GetLength(0); i++)
+fillArr(arr);
+
+void fillArr(double[,] arr)
+{
+ for (int i = 0; i < arr.GetLength(0); i++)
  {
   for (int j = 0; j < arr.GetLength(1); j++)
-   {
-    arr[i, j] = new Random().Next(1, 10);
-   }
+  {
+   arr[i, j] = new Random().Next(1, 10);
+  }
  }
 
-for (int i = 0; i < arr.GetLength(0); i++)
+ for (int i = 0; i < arr.GetLength(0); i++)
  {
   for (int j = 0; j < arr.GetLength(1); j++)
-   {
-    Write($"{arr[i, j]} ");
-   }
+  {
+   Write($"{arr[i, j]} ");
+  }
   Write("\n");
  }
+}
 
-WriteLine("|");
-WriteLine("V");
+WriteLine("|||||||");
 
+sortArr(arr);
+printArr(arr);
+
+void sortArr(double[,] arr)
+{
+ for (int i = 0; i < arr.GetLength(0); i++)
+ {
+  for (int j = 0; j < arr.GetLength(1); j++)
+  {
+   for (int x = 0; x < arr.GetLength(1) - 1; x++)
+   {
+    if (arr[i, x] < arr[i, x + 1])
+    {
+     double temp = arr[i, x + 1];
+     arr[i, x + 1] = arr[i, x];
+     arr[i, x] = temp;
+    }
+   }
+  }
+ }
+}
+
+void printArr(double[,] arr)
+{
+ for (int i = 0; i < arr.GetLength(0); i++)
+ {
+  for (int j = 0; j < arr.GetLength(1); j++)
+  {
+   Write(arr[i, j] + " ");
+  }
+  WriteLine();
+ }
+}
 
 WriteLine("---------------------");
+
+// Задача 56
 
