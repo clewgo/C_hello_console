eee40686b506521f163485008681a5a1f22016bd
Александр_П <mrtuzzz@gmail.com>
2022-12-10 21:45:50 +0500

    add example_8

--- example_7/Program.cs
+++ example_7/Program.cs
@@ -18,7 +18,7 @@ for (int i = 0; i < arr.GetLength(0); i++)
  {
   for (int j = 0; j < arr.GetLength(1); j++)
    {
-    arr[i, j] = new Random().Next(1, 5);
+    arr[i, j] = new Random().Next(1, 100);
    }
  }
 }
--- example_8/Program.cs
+++ example_8/Program.cs
@@ -1,5 +1,30 @@
 ﻿using static System.Console;
 Clear();
 
-// Задача 
+// Задача 54
+
+double[,] arr = new double[3, 4];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+ {
+  for (int j = 0; j < arr.GetLength(1); j++)
+   {
+    arr[i, j] = new Random().Next(1, 10);
+   }
+ }
+
+for (int i = 0; i < arr.GetLength(0); i++)
+ {
+  for (int j = 0; j < arr.GetLength(1); j++)
+   {
+    Write($"{arr[i, j]} ");
+   }
+  Write("\n");
+ }
+
+WriteLine("|");
+WriteLine("V");
+
+
+WriteLine("---------------------");
 
