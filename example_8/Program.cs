58dd16c057499796bbb345e8b36944af27eb108a
Александр_П <mrtuzzz@gmail.com>
2022-12-17 13:47:34 +0500

    add example_8 update

--- example_8/Program.cs
+++ example_8/Program.cs
@@ -152,3 +152,94 @@ WriteLine("---------------------");
 
 // Задача 60
 
+int[,,] arrX = GetArr(new int[] { 2, 2, 2 }, 10, 99);
+PrintX(arrX);
+
+int[,,] GetArr(int[] size, int min, int max)
+{
+ int[,,] resultX = new int[size[0], size[1], size[2]];
+ for (int i = 0; i < resultX.GetLength(0); i++)
+ {
+  for (int j = 0; j < resultX.GetLength(1); j++)
+  {
+   int k = 0;
+   while (k < resultX.GetLength(2))
+   {
+    int element = new Random().Next(min, max + 1);
+    if (FindEl(resultX, element)) continue;
+    resultX[i, j, k] = element;
+    k++;
+   }
+  }
+ }
+ return resultX;
+}
+
+bool FindEl(int[,,] arrX, int el)
+{
+ for (int i = 0; i < arrX.GetLength(0); i++)
+ {
+  for (int j = 0; j < arrX.GetLength(1); j++)
+  {
+   for (int k = 0; k < arrX.GetLength(2); k++)
+   {
+    if (arrX[i, j, k] == el) return true;
+   }
+  }
+ }
+ return false;
+}
+
+void PrintX(int[,,] arrX)
+{
+ for (int i = 0; i < arrX.GetLength(0); i++)
+ {
+  for (int j = 0; j < arrX.GetLength(1); j++)
+  {
+   for (int k = 0; k < arrX.GetLength(2); k++)
+   {
+    Console.Write($"{arrX[i, j, k]} ({i},{j},{k}) ");
+   }
+   Console.WriteLine();
+  }
+ }
+}
+
+WriteLine("---------------------");
+
+// Задача 62
+
+int[,] spiral = new int[4, 4];
+
+int temp1 = 1;
+int r = 0;
+int m = 0;
+
+while (temp1 <= spiral.GetLength(0) * spiral.GetLength(1))
+{
+ spiral[r, m] = temp1;
+ temp1++;
+ if (r <= m + 1 && r + m < spiral.GetLength(1) - 1)
+  m++;
+ else if (r < m && r + m >= spiral.GetLength(0) - 1)
+  r++;
+ else if (r >= m && r + m > spiral.GetLength(1) - 1)
+  m--;
+ else
+  r--;
+}
+
+WriteArray(spiral);
+
+void WriteArray(int[,] arrY)
+{
+ for (int i = 0; i < arrY.GetLength(0); i++)
+ {
+  for (int j = 0; j < arrY.GetLength(1); j++)
+  {
+   if (arrY[i, j] / 10 <= 0) Write($"0{arrY[i, j]} ");
+   else Write($"{arrY[i, j]} ");
+  }
+  WriteLine();
+ }
+}
