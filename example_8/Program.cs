b61fc22446700662165c96fe59697737da5a14ee
Александр_П <mrtuzzz@gmail.com>
2022-12-17 13:30:57 +0500

    add example_8 update

--- example_8/Program.cs
+++ example_8/Program.cs
@@ -67,3 +67,88 @@ WriteLine("---------------------");
 
 // Задача 56
 
+fillArr(arr);
+
+double x = 0;
+double y = 0;
+double z = 0;
+
+for (int i = 0; i < arr.GetLength(0); i++)
+ {
+  x = 0;
+  for (int j = 0; j <= arr.GetLength(0); j++)
+   {
+    x += arr[i, j];
+   }
+  if (x < z || i == 0) 
+  {
+   z = x;
+   y = i + 1;
+  }
+ }
+
+ WriteLine($"С наименьшей суммой элементов: {y} строка");
+
+WriteLine("---------------------");
+
+// Задача 58
+
+int[,] arr1 = new int[2, 2];
+CreateArr(arr1);
+WriteLine($"Первая матрица:");
+WriteArr(arr1);
+
+int[,] arr2 = new int[2, 2];
+CreateArr(arr2);
+WriteLine($"Вторая матрица:");
+WriteArr(arr2);
+
+int[,] result = new int[2, 2];
+
+MultiplyMatrix(arr1, arr2, result);
+WriteLine($"Матрица произведения:");
+WriteArr(result);
+
+void MultiplyMatrix(int[,] arr1, int[,] arr2, int[,] result)
+{
+ for (int i = 0; i < result.GetLength(0); i++)
+ {
+  for (int j = 0; j < result.GetLength(1); j++)
+  {
+   int sum = 0;
+   for (int k = 0; k < arr1.GetLength(1); k++)
+   {
+    sum += arr1[i, k] * arr2[k, j];
+   }
+   result[i, j] = sum;
+  }
+ }
+}
+
+void WriteArr(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+  for (int j = 0; j < array.GetLength(1); j++)
+  {
+   Write(array[i, j] + " ");
+  }
+  WriteLine();
+ }
+}
+
+void CreateArr(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+  for (int j = 0; j < array.GetLength(1); j++)
+  {
+   array[i, j] = new Random().Next(1, 10);
+  }
+ }
+}
+
+WriteLine("---------------------");
+
+// Задача 60
+
