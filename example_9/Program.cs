479f2be41f9e06d80917c3d18a1c1f005a7277f3
Александр_П <mrtuzzz@gmail.com>
2022-12-17 15:26:35 +0500

    add example_9 update

--- example_9/Program.cs
+++ example_9/Program.cs
@@ -3,3 +3,51 @@ Clear();
 
 // Задача 64
 
+Write("Задайте значение N: ");
+int n = int.Parse(ReadLine()!);
+
+string PrintN(int n)
+{
+ if (n == 1) return n.ToString();
+ return (n + " " + PrintN(n - 1));
+}
+Write(PrintN(n));
+WriteLine();
+
+WriteLine("---------------------");
+
+// Задача 66
+
+Write("Задайте значение от: ");
+int mm = int.Parse(ReadLine()!);
+
+Write("Задайте значение до: ");
+int nn = int.Parse(ReadLine()!);
+
+int Summ(int start, int end)
+{
+ if (start == end) return start;
+ if (start > end) return start + Summ(end, start - 1);
+ return end + Summ(start, end - 1);
+}
+
+WriteLine($"Сумма всех элементов от {mm} до {nn} = {Summ(mm, nn)}");
+
+WriteLine("---------------------");
+
+// Задача 68
+
+Write("Задайте значение M: ");
+int mmm = int.Parse(ReadLine()!);
+
+Write("Задайте значение N: ");
+int nnn = int.Parse(ReadLine()!);
+
+int Ackerman(int mmm, int nnn)
+{
+  if (mmm == 0) return nnn + 1;
+  else if (nnn == 0) return Ackerman(mmm - 1, 1);
+  else return Ackerman(mmm - 1, Ackerman(mmm, nnn - 1));
+}
+
+WriteLine($"A(M,N) = {Ackerman(mmm,nnn)}");
